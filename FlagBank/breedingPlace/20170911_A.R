1ba4ec9b54a79f924e518bbcbf80f2648dc9d02f
krondor <krondor@example.com>
2017-09-30 18:58:04 +0900

    seqHaunt max

--- FlagBank/breedingPlace/20170911_A.R
+++ FlagBank/breedingPlace/20170911_A.R
@@ -9,61 +9,105 @@ setwd("..")
 FB <- getFlagBank()
 setwd(curWd)
 
-fLine <- FB$zh[,1]
+glm.out <- createProbReg(FB)
 
-hSpan <- 100:nrow(FB$zh)
-divBase <- 8
-flag <- fLine %% divBase
+# 발생가능 범위를 생각하고
 
+for( colIdx in 1:6 ){
+	for( sMaxIdx in seq(3,50,3) ){
+		curDnaH <- FB$zh[,colIdx]
+		curDnaH.range <- range(curDnaH)
+		curDna.code <- curDnaH.range[1]:curDnaH.range[2]
 
+		charactList <- list()
+		charactList[[(1+length(charactList))]]	<- charactModu(2)
+		charactList[[(1+length(charactList))]]	<- charactModu(5)
+		charactList[[(1+length(charactList))]]	<- charactModu(7)
+		charactList[[(1+length(charactList))]]	<- charactIntDiv(pBase=3)
+		charactList[[(1+length(charactList))]]	<- charactIntDiv(pBase=6)
 
-log.txt <- "./log/sampleMtx.txt"
-k.FLogStr("",pFile=log.txt)
+		freq.tbl <- table(curDnaH)
+		freq.NomiNum <- 7
+		freq.NomiVal <- as.integer( names(freq.tbl)[ order( freq.tbl ,decreasing=T )[1:freq.NomiNum] ] )
+		freq.NomiPos <- mapply(function(p){ which(curDna.code==p) } ,freq.NomiVal)
 
-cName <- c("output","mean","mean.diff","seqHaunt")
-statDf <- data.frame( output=integer(0) ,mean=numeric(0) ,mean.diff=numeric(0) ,seqHaunt=integer(0) )
+		hSpan <- 100:length(curDnaH)
+		nomineeList <- list()
+		hitOrd	<- matrix( 0 ,nrow=length(hSpan) ,ncol=2)
+		for( idx in seq_len(length(hSpan)) ) { # idx <- 1
+			hIdx <- hSpan[idx]
+			nominee <- nominateCode( pCode=curDna.code ,pH=curDnaH[1:(hIdx-1)] 
+								,pCharactList=charactList
+								,pProbFit=glm.out
+								,pSeqHauntMax=sMaxIdx
+							)
+			
+			hitPos <- which(curDnaH[hIdx]==curDna.code[order(nominee$finalProb,decreasing=T)])
+			hitOrd[idx,1] <- hitPos
+			hitOrd[idx,2] <- hitOrd[idx,1]*100 %/% length(curDna.code)
 
-for( cSize in 1:(divBase-1) ){
-	combMtx <- combinations( divBase ,cSize )
-	combMtx <- combMtx-1	# flag는 나머지들의 값이므로.
+			nomineeList[[(1+length(nomineeList))]] <- nominee
 
-	for( combIdx in seq_len(nrow(combMtx)) ){
-		bFlag <- flag %in% combMtx[combIdx,]
-		rObj <- getFlagStatSample( bFlag ,hSpan )
-		k.FLogStr(sprintf("\\n cSize:%d combIdx:%d (mean:%03.f%%)----------------"
-					,cSize ,combIdx ,100*mean(rObj$sampleMtx[,"mean"])
-				)
-				,pConsole=T ,pFile=log.txt
-			)
-		statDf <- rbind( statDf ,rObj$sampleMtx )
-	}	# for(combIdx)
-} # for(cSize)
+		}
 
-
-glm.out <- glm( output~poly(mean,4)+poly(mean.diff,4)+poly(seqHaunt,4) 
-					,data=statDf ,family=binomial
-				)
-seqHaunt <- sort(unique(statDf$seqHaunt))
-seqHaunt.col <- terrain.colors(length(seqHaunt))
-
-xRange <- range( statDf$mean )
-for( shIdx in seq_len(length(seqHaunt)) ){
-
-	rDataFlag <- statDf$seqHaunt==seqHaunt[shIdx]
-
-	predDf <- data.frame( mean=seq( xRange[1] ,xRange[2] ,0.01 ) )
-	predDf$mean.diff	<- rep( mean(statDf$mean.diff[rDataFlag]) ,nrow(predDf) )
-	predDf$seqHaunt		<- rep( seqHaunt[shIdx] ,nrow(predDf) )
-	glm.pred <- predict( glm.out ,predDf ,type="response" )
+		k.FLogStr(sprintf("colIdx:%d  sMaxIdx:%d",colIdx,sMaxIdx))
+		k.FLogStr(sprintf("    lvl3:%4.2f   lvl4:%4.2f   lvl5:%4.2f   lvl6:%4.2f   lvl7:%4.2f   lvl8:%4.2f"
+						,mean(hitOrd[,1]<4)	,mean(hitOrd[,1]<5)	,mean(hitOrd[,1]<6)
+						,mean(hitOrd[,1]<7)	,mean(hitOrd[,1]<8)	,mean(hitOrd[,1]<9)
+					),pTime=F)
 	
-	if( shIdx == 1 ){
-		plot( predDf$mean ,glm.pred
-				,xlim=xRange ,ylim=c(-0.1,1.1)
-				,pch="+" ,col=seqHaunt.col[shIdx] ,type="l" )
+	}	# for(sMaxIdx)
+}	# for(colIdx)
+
+
+# --[ Report ]----------------------------------
+curDna.codeCol <- terrain.colors( length(curDna.code) )
+stdCode <- curDnaH[hSpan]
+
+repIdx <- 3
+nomiObj <- nomineeList[[repIdx]]
+winnerOrd <- order(nomiObj$finalProb,decreasing=T)
+
+k.FLogStr( sprintf("%d th Report",repIdx ) )
+k.FLogStr( sprintf("  stdCode:%d  winner :%s"
+				,stdCode[repIdx] ,paste(curDna.code[winnerOrd[1:5]],collapse=" ") 
+			) )
+
+str <- apply( nomiObj$probMtx ,1 ,function(p){ paste(sprintf("%4.1f",p),collapse=" ") } )
+str <- paste( "  " ,str ,collapse="\n")
+k.FLogStr( str ,pTime=F )
+
+ordHisMtx <- nomiObj$probMtx
+ordHisMtx.rnk <- ordHisMtx
+ordHisMtx.one <- ordHisMtx
+for( rIdx in 1:nrow(ordHisMtx) ){
+
+	probLine <- nomiObj$probMtx[rIdx,]
+	if( rIdx == 1){
+		ordHisMtx[rIdx,] <- probLine
 	} else {
-		lines( predDf$mean ,glm.pred ,col=seqHaunt.col[shIdx] )
+		ordHisMtx[rIdx,] <- probLine * ordHisMtx[(rIdx-1),]
 	}
-	
+
+	ordHisMtx.one[rIdx,] <- ordHisMtx[rIdx,] / max(ordHisMtx[rIdx,])
+	ordHisMtx.rnk[rIdx,] <- rank( ordHisMtx[rIdx,] )
 }
+
+for( wIdx in length(winnerOrd):1 ){
+	if( wIdx==length(winnerOrd) ){
+		plot(ordHisMtx.rnk[,winnerOrd[wIdx]] 
+			,ylim=c(0,length(winnerOrd)+1) ,type="l" ,col=curDna.codeCol[wIdx] )
+	} else {
+		lines( 1:nrow(ordHisMtx.rnk) ,ordHisMtx.rnk[,wIdx] ,col=curDna.codeCol[wIdx] )
+	}
+}
+
+# winner
+points( 1:nrow(ordHisMtx.rnk) ,ordHisMtx.rnk[,winnerOrd[1]] ,col="blue" )
+# stdCode
+stdIdx <- which(curDna.code == stdCode[repIdx])
+lines( 1:nrow(ordHisMtx.rnk) ,ordHisMtx.rnk[,stdIdx] ,col="red" )
+
+
 
 
--- FlagBank/breedingPlace/20170911_A_H.R
+++ FlagBank/breedingPlace/20170911_A_H.R
@@ -28,9 +28,178 @@ getFlagStatSample <- function( pFlag ,pHSpan ){
 					} # for
 
 					return( list( sampleMtx=mtx ) )
-				}
+}	# getFlagStatSample
+
+
+getStatCharact <- function( pFlag ,pNextVal ){
+
+						revFlag <- pFlag[length(pFlag):1]
+						meanVal <- mean(pFlag)
+
+						cnt <- 0
+						for( cntIdx in 2:length(revFlag) ){
+							if( revFlag[cntIdx]==revFlag[1] ){
+								cnt <- cnt+1
+							} else {
+								break
+							}
+						}	# QQE : 데이터 생성 확인. 
+						seqHaunt <- ifelse( revFlag[1] , cnt+1 , -cnt )
+
+						rObj <- data.frame(	output = pNextVal
+								,mean		= meanVal
+								,mean.diff	= meanVal - mean(revFlag[1:20])
+								,seqHaunt	= seqHaunt
+							)
+						return(rObj)
+}	# getStatCharact
+
+#		- pProbSoften : 예상 확률이 발생빈도를 너무 무시하지 않게...
+#						(발생빈도 낮은 코드들의 장기 미발생 깽판 방지)
+#		- pSeqHauntMax : seqHaunt
+nominateCode <- function( pCode ,pH ,pCharactList ,pProbFit ,pProbSoften=F ,pSeqHauntMax=20 ){
+
+					probAdjust <- T
+					probMtx <- matrix(1,nrow=length(pCharactList),ncol=length(pCode))
+					probMtx.mean <- probMtx	;probMtx.meanDiff <- probMtx	;probMtx.seqHaunt <- probMtx
+					colnames(probMtx) <- pCode
+					for( chIdx in seq_len(length(pCharactList)) ){ # chIdx <- 4
+
+						statCharact <- NULL
+						histCh <- pCharactList[[chIdx]]$getCharact( pH )
+						for( cdIdx in pCharactList[[chIdx]]$codeRange ){	# cdIdx <- pCharactList[[chIdx]]$codeRange[2]
+							tFlag <- histCh == cdIdx
+							if( is.null(statCharact) ){
+								statCharact <- getStatCharact( tFlag[1:length(tFlag)] ,TRUE )
+							} else {
+								statCharact <- rbind( statCharact 
+														,getStatCharact( tFlag[1:length(tFlag)] ,TRUE ) 
+													)
+							}
+						}
+						if( !is.null(pSeqHauntMax) ){
+							statCharact$seqHaunt <- ifelse( statCharact$seqHaunt > pSeqHauntMax 
+																,pSeqHauntMax ,statCharact$seqHaunt )
+							statCharact$seqHaunt <- ifelse( statCharact$seqHaunt < -pSeqHauntMax 
+																,-pSeqHauntMax ,statCharact$seqHaunt )
+						}
+
+						prob <- predict( pProbFit ,statCharact[,c("mean","mean.diff","seqHaunt")] ,type="response" )
+						if( pProbSoften )
+							prob <- prob * statCharact$mean
+
+						if( probAdjust ){
+							probMax <- max(prob)
+							prob <- prob / probMax	# 모든 Prob가 0인 경우는 없겠지.
+						}
+
+						codeCh <- pCharactList[[chIdx]]$getCharact( pCode )
+						indices <- mapply(function(p){which(p==pCharactList[[chIdx]]$codeRange)} ,codeCh)
+						probMtx[chIdx,]			<- prob[indices]
+						probMtx.mean[chIdx,]	<- statCharact$mean[indices]
+						probMtx.meanDiff[chIdx,]<- statCharact$mean.diff[indices]
+						probMtx.seqHaunt[chIdx,]<- statCharact$seqHaunt[indices]
+					}
+
+					finalProb <- apply( probMtx ,2 ,prod)
+
+					rObj <- list( nominee=pCode[which(finalProb==max(finalProb))] )
+					rObj$finalProb <- finalProb
+					rObj$probMtx <- probMtx
+					rObj$probMtx.mean		<- probMtx.mean
+					rObj$probMtx.meanDiff	<- probMtx.meanDiff
+					rObj$probMtx.seqHaunt	<- probMtx.seqHaunt
+					return( rObj )
+
+}	# nominateCode
+
+
+charactModu <- function( pBase=8 ){	# modulo
+					rObj <- list(idStr=sprintf("charactModu.%02d",pBase))
+					rObj$base <- pBase
+					rObj$codeRange <- 0:(pBase-1)
+					rObj$getCharact <- function( pCode ){
+								return( pCode %% pBase )
+							}
+					return( rObj )
+}	# charactModu
+
+charactIntDiv <- function( pBase=5 ,pMaxCode=10 ){	# Integer Division
+					rObj <- list(idStr=sprintf("charactIntDiv.%02d.%02d",pBase,pMaxCode))
+					rObj$base <- pBase
+					rObj$codeRange <- 0:pMaxCode
+					rObj$getCharact <- function( pCode ){
+								intDiv <- pCode %/% pBase
+								return( ifelse(intDiv>pMaxCode,pMaxCode,intDiv) )
+							}
+					return( rObj )
+}
+
+
+createProbReg <- function( pFB ){
+						fLine <- pFB$zh[,1]
+
+						hSpan <- 100:nrow(pFB$zh)
+						divBase <- 8
+						flag <- fLine %% divBase
 
 
 
+						log.txt <- "./log/sampleMtx.txt"
+						k.FLogStr("",pFile=log.txt)
+
+						cName <- c("output","mean","mean.diff","seqHaunt")
+						statDf <- data.frame( output=integer(0) ,mean=numeric(0) ,mean.diff=numeric(0) ,seqHaunt=integer(0) )
+
+						for( cSize in 1:(divBase-1) ){
+							combMtx <- combinations( divBase ,cSize )
+							combMtx <- combMtx-1	# flag는 나머지들의 값이므로.
+
+							for( combIdx in seq_len(nrow(combMtx)) ){
+								bFlag <- flag %in% combMtx[combIdx,]
+								rObj <- getFlagStatSample( bFlag ,hSpan )
+								k.FLogStr(sprintf("\\n cSize:%d combIdx:%d (mean:%03.f%%)----------------"
+											,cSize ,combIdx ,100*mean(rObj$sampleMtx[,"mean"])
+										)
+										,pConsole=F ,pFile=log.txt
+									)
+								statDf <- rbind( statDf ,rObj$sampleMtx )
+							}	# for(combIdx)
+						} # for(cSize)
+
+
+						glm.out <- glm( output~poly(mean,4)+poly(mean.diff,4)+poly(seqHaunt,4) 
+											,data=statDf ,family=binomial
+										)
+
+						if( T ) # 일반적으론 여기서 끝.
+							return( glm.out )
+
+						# 이하는 데이터 검토용 코드
+						seqHaunt <- sort(unique(statDf$seqHaunt))
+						seqHaunt.col <- terrain.colors(length(seqHaunt))
+
+						xRange <- range( statDf$mean )
+						for( shIdx in seq_len(length(seqHaunt)) ){
+
+							rDataFlag <- statDf$seqHaunt==seqHaunt[shIdx]
+
+							predDf <- data.frame( mean=seq( xRange[1] ,xRange[2] ,0.01 ) )
+							predDf$mean.diff	<- rep( mean(statDf$mean.diff[rDataFlag]) ,nrow(predDf) )
+							predDf$seqHaunt		<- rep( seqHaunt[shIdx] ,nrow(predDf) )
+							glm.pred <- predict( glm.out ,predDf ,type="response" )
+
+							if( shIdx == 1 ){
+								plot( predDf$mean ,glm.pred
+										,xlim=xRange ,ylim=c(-0.1,1.1)
+										,pch="+" ,col=seqHaunt.col[shIdx] ,type="l" )
+							} else {
+								lines( predDf$mean ,glm.pred ,col=seqHaunt.col[shIdx] )
+							}
+						}
+
+						return( glm.out )
+
+}	# createProbReg
 
 
