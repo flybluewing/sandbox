b0ef9715699e972d0dfd3edc52ce438fbf9c6253
krondor <krondor@example.com>
2017-11-16 19:23:58 +0900

    done

--- FlagBank/breedingPlace/20171116_A_H.R
+++ FlagBank/breedingPlace/20171116_A_H.R
@@ -1,5 +1,22 @@
 # 20171116_A_H.R
 
+# 일치하는 갯수
+getMatchLst <- function( pCode ,pArea ){
+
+	matchLst <- list()
+	for( rIdx in 1:nrow(pArea) ){
+		# pCode 내에 같은 값이 있을 경우를 위해 for문 적용
+		flag <- sapply( pCode ,function(p){p%in%pArea[rIdx,]} )
+		if( any(flag) ){
+			matchObj <- list(hIdx=rIdx,flag=which(flag))
+			matchLst[[1+length(matchLst)]] <- matchObj
+		}
+	} # for(rIdx)
+
+	return(matchLst)
+} # getMatchLst()
+
+# pCode의 각 요소가 얼마만에 재현된 것인지.
 #   pArea : matrix
 getReboundLst <- function( pCode ,pArea ,pSearchFirst=F ){
 
