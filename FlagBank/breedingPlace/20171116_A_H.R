22744abbf4fbae5e117c25feb48c3e858bd84943
krondor <krondor@example.com>
2017-11-17 18:44:24 +0900

    dome

--- FlagBank/breedingPlace/20171116_A_H.R
+++ FlagBank/breedingPlace/20171116_A_H.R
@@ -8,13 +8,29 @@ getMatchLst <- function( pCode ,pArea ){
 		# pCode 내에 같은 값이 있을 경우를 위해 for문 적용
 		flag <- sapply( pCode ,function(p){p%in%pArea[rIdx,]} )
 		if( any(flag) ){
-			matchObj <- list(hIdx=rIdx,flag=which(flag))
+			matchObj <- list(hIdx=rIdx,fIdx=which(flag))
 			matchLst[[1+length(matchLst)]] <- matchObj
 		}
 	} # for(rIdx)
 
 	return(matchLst)
 } # getMatchLst()
+
+# getMatchLst() 와 비슷하나, 코드의 위치까지 동일한 경우를 찾는다.
+getMatchLst.fixed <- function( pCode ,pArea ){
+
+	matchLst <- list()
+	for( rIdx in 1:nrow(pArea) ){
+		indices <- which(pArea[rIdx,]==pCode)
+		if( 0<length(indices) ){
+			matchLst[[1+length(matchLst)]] <-
+				list( hIdx=rIdx ,fIdx=indices )
+		}
+	} # for(rIdx)
+
+	return( matchLst )
+} # getMatchLst.fixed()
+
 
 # pCode의 각 요소가 얼마만에 재현된 것인지.
 #   pArea : matrix
--- FlagBank/breedingPlace/20171116_A_exp.R
+++ FlagBank/breedingPlace/20171116_A_exp.R
@@ -2,9 +2,19 @@
 pZh <- as.matrix(FB$zh)
 pCode <- pZh[20,]
 pArea <- pZh[1:19,]
-pSearchFirst <- F
 
 
+getMatchLst.fixed <- function( pCode ,pArea ){
 
+	matchLst <- list()
+	for( rIdx in 1:nrow(pArea) ){
+		indices <- which(pArea[rIdx,]==pCode)
+		if( 0<length(indices) ){
+			matchLst[[1+length(matchLst)]] <-
+				list( hIdx=rIdx ,fIdx=indices )
+		}
+	} # for(rIdx)
 
+	return( matchLst )
+} # getMatchLst.fixed()
 
