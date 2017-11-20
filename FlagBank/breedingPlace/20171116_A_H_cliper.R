a0ef0efd4080d42fccf2d9e21d4ae1fae02b9ac2
krondor <krondor@example.com>
2017-11-20 14:39:00 +0900

    done

--- FlagBank/breedingPlace/20171116_A_H_cliper.R
+++ FlagBank/breedingPlace/20171116_A_H_cliper.R
@@ -7,6 +7,78 @@
 # clp.dumNum$byBase( zoidMtx )
 # clp.dumNum$byLate( zoidMtx ,baseH )
 
+cliper.backStep <- function( pBaseZh ,pThld=4 ,pSanc=10 ,pBackStep=1 ){
+    rObj <- list( idStr="backStep" ,thld=pThld ,sanc=pSanc )
+    rObj$backStep <- pBackStep
+    rObj$getBaseH <- function( pZoidH ){
+        stepDiff <- pZoidH[1:(nrow(pZoidH)-rObj$backStep),] 
+            - pZoidH[(rObj$backStep+1):nrow(pZoidH)]
+        return( abs( stepDiff ) )
+    } # rObj$getBaseH()
+    rObj$baseH <- rObj$getBaseH( pBaseZh )
+    byLate <- function( pZoidMtx ,pZoidH ,pDebugInfo=F ){
+        lateZoid <- pZoidH[(nrow(pZoidH)-rObj$backStep+1),]
+        codeMtx <- t(apply( pZoidMtx ,1 ,function(p){abs(p-lateZoid)}))
+        baseH <- rObj$getBaseH( pZoidH ) # 상위 손실 발생영역이 제거됨 유의.
+        scanSpan <- (nrow(baseH)-rObj$sanc+1 ):nrow(baseH)
+        rst <- apply( codeMtx ,1 ,function(pCode){
+                    for( rIdx in scanSpan ){
+                        if( rObj$thld <= sum(pCode==baseH[rIdx,] ,na.rm=T) )
+                            return( rIdx+rObj$backStep )
+                    } # for(rIdx)
+                    return( NA )
+                })
+        if( pDebugInfo ) {    return( rst )
+        } else {    return( !is.na(rst) ) }
+    } # byLate()
+    byBase <- function( pZoidMtx ,pZoidH ,pDebugInfo=F ){
+        #   - pZoidH : 가장 최신 H. 여기선 사용치 않음.
+        lateZoid <- pZoidH[(nrow(pZoidH)-rObj$backStep+1),]
+        codeMtx <- t(apply( pZoidMtx ,1 ,function(p){abs(p-lateZoid)}))
+        rst <- apply( codeMtx ,1 ,function(pCode){
+                    for( rIdx in 1:nrow(rObj$baseH) ){
+                        if( rObj$thld <= sum(pCode==rObj$baseH[rIdx,] ,na.rm=T) )
+                            return( rIdx+rObj$backStep )
+                    } # for(rIdx)
+                    return( NA )
+                })
+        if( pDebugInfo ) {    return( rst )
+        } else {    return( !is.na(rst) ) }
+    } # byBase()
+    rObj$byLate <- byLate
+    rObj$byBase <- byBase
+    rObj$report <- function( pFile="./report/cliperReport.txt" ){
+
+        k.FLogStr(sprintf("cliper id : %s",rObj$idStr) ,pFile=pFile)
+        matchLst <- list()
+        for( hIdx in 2:nrow(rObj$baseH) ){
+            ml <- getMatchLst.fixed( rObj$baseH[hIdx,] ,rObj$baseH[1:(hIdx-1),,drop=F] )
+            for( idx in seq_len(length(ml)) ){
+                matLen <- length(ml[[idx]]$fIdx)
+                if(rObj$thld<=matLen){
+                    matchLst[[1+length(matchLst)]] <- 
+                        c( hIdx ,ml[[idx]]$hIdx ,matLen)
+                }
+            } #for(idx)
+        }
+
+        mtx <- do.call( rbind ,matchLst )    # curIdx, matchIdx, matchNum
+        k.FLog( sort(mtx[,1]-mtx[,2])[1:10] ,pFile=pFile )
+
+        dupIndices <- sort(unique( c(mtx[,1],mtx[,2]) ))    # 398
+        k.FLogStr(sprintf("dupIndices : %d of %d",length(dupIndices),nrow(rObj$baseH))
+                     ,pFile=pFile )
+
+        testFlag <- rep(0,nrow(zh))
+        testFlag[dupIndices] <- 1
+        seqObj <- k.seq(testFlag)
+        zeroFlag <- seqObj$seqCntMtx[,"val"]==0
+        k.FLog( table(seqObj$seqCntMtx[!zeroFlag,"cnt"]) ,pFile=pFile )
+
+    } # rObj$report()
+    return( rObj )
+} # cliper.backStep()
+
 cliper.stepWidth <- function( pBaseZh ,pThld=4 ,pSanc=10 ){
     rObj <- list( idStr="stepWidth" ,thld=pThld ,sanc=pSanc )
     rObj$getBaseH <- function( pZoidH ){ 
@@ -176,10 +248,11 @@ cliper.dupNum <- function( pBaseZh ,pThld=4 ,pSanc=2 ){
     byLate <- function( pZoidMtx ,pZoidH ,pDebugInfo=F ){
         #   - pDebugInfo가 F이면 clip여부 T/F만, T이면 디버깅 정보 반환.
         codeMtx <-pZoidMtx
-        scanSpan <- (nrow(pZoidH)-rObj$sanc+1 ):nrow(pZoidH)
+        baseH <- rObj$getBaseH( pZoidH )
+        scanSpan <- (nrow(baseH)-rObj$sanc+1 ):nrow(baseH)
         rst <- apply( codeMtx ,1 ,function(pCode){
                     for( rIdx in scanSpan ){
-                        flag <- sapply(pCode,function(p){p%in%pZoidH[rIdx,]})
+                        flag <- sapply(pCode,function(p){p%in%baseH[rIdx,]})
                         if( rObj$thld<=sum(flag) )
                             return( rIdx )
                     } # for(rIdx)
