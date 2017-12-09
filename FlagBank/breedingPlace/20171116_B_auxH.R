9b280dc3974f3763c880770691681e39fc4fc63e
krondor <krondor@example.com>
2017-12-09 19:21:30 +0900

    .

---
+++ FlagBank/breedingPlace/20171116_B_auxH.R
@@ -0,0 +1,13 @@
+# 20171116_B_auxH.R юс╫ц
+
+source("20171116_A_H.R")
+source("20171116_A_H_cliper.R")
+source("20171116_B_H.R")
+myObj <- load( "Obj_deskObj.save" )
+#   deskObj
+#    - zhF testSpan allZoidMtx 
+#    - indices.pool indices.h indices.all indices.flag indices.zoidMtx
+
+
+zhF <- deskObj$zhF
+allZoidMtx <- deskObj$allZoidMtx
