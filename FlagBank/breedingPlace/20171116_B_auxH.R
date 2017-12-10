fb0cb3cdc791bc20c5e71d2a94634335a164afc9
krondor <flybluewing@naver.com>
2017-12-10 16:44:09 +0900

    .

--- FlagBank/breedingPlace/20171116_B_auxH.R
+++ FlagBank/breedingPlace/20171116_B_auxH.R
@@ -11,3 +11,51 @@ myObj <- load( "Obj_deskObj.save" )
 
 zhF <- deskObj$zhF
 allZoidMtx <- deskObj$allZoidMtx
+
+flag.z <- allZoidMtx[,2] %% 5
+
+flag <- zhF[,2] %% 5
+seqObj <- k.seq(flag)
+seqCntMtx <- seqObj$seqCntMtx
+seqIdx <- which(seqCntMtx[,"cnt"]>1)
+
+resLst <- list()
+for( hIdx in 500:length(flag) ){
+	seqObj <- k.seq( flag[1:(hIdx-1)] )
+	resObj <- list( hIdx=hIdx )
+	resObj$hit <- flag[hIdx]!=flag[hIdx-1]
+	resObj$rmCnt <- sum(flag.z == flag[hIdx-1])
+	resLst[[1+length(resLst)]] <- resObj
+}
+resLst.base <- resLst
+
+resLst <- list()
+for( hIdx in 500:length(flag) ){
+	seqObj <- k.seq( flag[1:(hIdx-1)] )
+	resObj <- list( hIdx=hIdx )
+	resObj$hit <- flag[hIdx]!=flag[hIdx-1]
+	resObj$rmCnt <- sum(flag.z == flag[hIdx-1])
+	resLst[[1+length(resLst)]] <- resObj
+}
+
+
+
+#	연속 발생은 없다.
+#		- 다음 숫자는 지금 숫자와는 다르다는 전제
+#	연속 발생 후, 바로 다른 연속발생이 발생치는 않는다.
+#		- 2,2,2,3 으로 끝난 상태에서의 다음 숫자는 3이 아니다
+#	연속 발생 종료 후 발생하는 숫자는 이전 연속발생 종료다음 숫자와 다르다.
+#		- 현재 연속발생 숫자와 이전 연속발생 종료 다음 숫자를 제외하자.
+#	연속 발생 길이는 이전 연속발생 길이와 다르다.
+#		- 해당 길이 상태에서 연속발생 가능성 낮다. 그 다음다음 연속은 필히 발생.
+
+
+
+
+codeType <- sort(unique(flag))
+cName <- c("seqLen","nextVal")
+seqInfoMtx <- matrix(0,nrow=length(codeType),ncol=length(cName))	;colnames(seqInfoMtx) <- cName
+for( hIdx in 2:nrow(hSeqCntMtx) ){
+	
+}
+
