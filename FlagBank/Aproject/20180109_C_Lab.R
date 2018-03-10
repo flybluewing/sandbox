862919a6662d197d4da70a8ea097729ad5c71098
krondor <krondor@example.com>
2018-03-11 01:34:57 +0900

    .

--- FlagBank/Aproject/20180109_C_Lab.R
+++ FlagBank/Aproject/20180109_C_Lab.R
@@ -10,71 +10,94 @@ source("20180109_C_H.R")
 
 saveId <- "0127_23"
 load(sprintf("./save/Obj_gEnv%s.save",saveId))
-# load(sprintf("./save/Obj_fRstLst%s.save",saveId))
-# load(sprintf("./save/Obj_remLst%s.save",saveId))
+load(sprintf("./save/Obj_fRstLst%s.save",saveId))
+load(sprintf("./save/Obj_remLst%s.save",saveId))
 
+remLst
 
-
-cfObjLst <- NULL
-encValLst <- list()
-for( tIdx in 10:nrow(gEnv$zhF) ){	# lastZoid 기반 동작들 때문에 1부터 시작은 의미없다.
-	tEnv <- gEnv
-	tEnv$zhF <- gEnv$zhF[1:(tIdx-1),]
-	tEnv$allZoidMtx <- gEnv$zhF[tIdx,,drop=F]
-	
-	cfObjLst <- getCFLst.base( tEnv )
-	for( lIdx in seq_len(length(cfObjLst)) ){
-		cfObj <- cfObjLst[[lIdx]]
-		if( is.null(encValLst[[cfObj$idStr]]) ){
-			encValLst[[cfObj$idStr]] <- list()
-		}
-		encValLst[[cfObj$idStr]][[1+length(encValLst[[cfObj$idStr]])]] <- cfObj$enc( tEnv$allZoidMtx )[[1]]
-	}
+fltCnt <- rep( 0 ,nrow(gEnv$allZoidMtx) )
+for( remIdx in 1:length(remLst) ){
+	fltCnt[remLst[[remIdx]]] <- fltCnt[remLst[[remIdx]]] + 1
 }
 
-cfNames <- sapply(cfObjLst,function(p){p$idStr})
-pairNum <-	sapply( cfNames ,function(p){
-						# 각 encVal에서 중복쌍이 얼마나 많이 나오는지 측정
-						mtx <- evalScan.pair( encValLst ,p ,cfObjLst )
-						return( nrow(mtx) )
-					})
-names(pairNum) <- cfNames
-encVal.len <- length(encValLst[[1]])
 
-cfNameIdxMtx <- permutations( length(cfNames) ,2 )
-rstValLst <- list()
-for( cfIdx in 1:nrow(cfNameIdxMtx) ){
+chosenZ.idx <- which( fltCnt==0 )
+# chosenZ.idx <- sort(sample(chosenZ.idx,100))
+chosenZMtx <- gEnv$allZoidMtx[chosenZ.idx,]
 
-	k.FLogStr(sprintf("  starting cfIdx:%d",cfIdx))
-	
-	scanName <- cfNames[cfNameIdxMtx[cfIdx,]]
-	sbc.name <- scanName[1]
-	nzc.name <- scanName[2]
-	
-	testSpan <- 200:encVal.len
-	rstVal <- rep( NA ,length(testSpan) )
-	for( tIdx in 1:length(testSpan) ){
-		tIdx.r <- testSpan[tIdx]
-		valLst <- list()
-		valLst[[sbc.name]] <- encValLst[[sbc.name]][1:(tIdx.r-1)]
-		valLst[[nzc.name]] <- encValLst[[nzc.name]][1:(tIdx.r-1)]
-		rstObj <- evlScan( valLst ,sbc.name ,nzc.name ,cfObjLst )
-		if( !is.null(rstObj$lastZC) ){
-			stdVal <- encValLst[[nzc.name]][[tIdx.r]]
-			cfObj <- cfObjLst[[ cfNameIdxMtx[cfIdx,2] ]]	# nzc.name
-			rstVal[tIdx] <- cfObj$diffCnt( stdVal ,rstObj$lastZC )
-		}
-	}
+rstLst <- list()
 
-	rstValLst[[1+length(rstValLst)]] <- rstVal
-	
-} # cfIdx
+tStmp <- Sys.time()
+# ==================================================================
+banObj <- getCFltObj( gEnv )
+codeLst <- banObj$getCodeLst( chosenZMtx )
+tDiff <- Sys.time() - tStmp
+cat(sprintf("banObj %.1f%s\n",tDiff,units(tDiff)))
 
-lapply( rstValLst ,table )
+bRstObj <- ban.hntSameRow(banObj ,chosenZMtx ,pCodeLst=codeLst)
+rstIdStr <- sprintf("%s_base",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
 
-cnt4Zero <- sapply( rstValLst ,function(p){
-					ifelse( any(p==0,na.rm=T) ,table(p)["0"] ,NA )
-				})
-rstValMtx <- do.call( rbind ,rstValLst )
-rstVal.fltNum <- apply( rstValMtx ,2 ,function(p){sum(!is.na(p))})
+bRstObj <- ban.hntCrossDim(banObj ,chosenZMtx ,pCodeLst=codeLst)
+rstIdStr <- sprintf("%s_base",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
+bRstObj <- ban.multiDim(banObj ,chosenZMtx ,pCodeLst=codeLst)
+rstIdStr <- sprintf("%s_base",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
+bRstObj <- ban.throughH(banObj ,chosenZMtx ,pCodeLst=codeLst)
+rstIdStr <- sprintf("%s_base",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
+bRstObj <- ban.throughH2(banObj ,chosenZMtx ,pCodeLst=codeLst)
+rstIdStr <- sprintf("%s_base",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
+# ==================================================================
+banCmbObj <- getCFltCmbObj( gEnv )
+codeCmbLst <- banCmbObj$getCodeLst( chosenZMtx )
+tDiff <- Sys.time() - tStmp
+cat(sprintf("banCmbObj %.1f%s\n",tDiff,units(tDiff)))
+
+bRstObj <- ban.hntSameRow(banCmbObj ,chosenZMtx ,pCodeLst=codeCmbLst)
+rstIdStr <- sprintf("%s_comb",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
+bRstObj <- ban.hntCrossDim(banCmbObj ,chosenZMtx ,pCodeLst=codeCmbLst)
+rstIdStr <- sprintf("%s_comb",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
+bRstObj <- ban.multiDim(banCmbObj ,chosenZMtx ,pCodeLst=codeCmbLst)
+rstIdStr <- sprintf("%s_comb",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
+bRstObj <- ban.throughH(banCmbObj ,chosenZMtx ,pCodeLst=codeCmbLst)
+rstIdStr <- sprintf("%s_comb",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
+bRstObj <- ban.throughH2(banCmbObj ,chosenZMtx ,pCodeLst=codeCmbLst)
+rstIdStr <- sprintf("%s_comb",bRstObj$idStr)
+rstLst[[rstIdStr]] <- bRstObj$filtedIdx
+tDiff <- Sys.time() - tStmp
+cat(sprintf("%s %.1f%s\n",rstIdStr,tDiff,units(tDiff)))
+
 
