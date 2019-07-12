4e0b6c36d512dc3f7ca9aee3a745b4a9959860f6
krondor <krondor@example.com>
2019-07-12 17:09:42 +0900

    .

--- FlagBank/Bproject/B.R
+++ FlagBank/Bproject/B.R
@@ -1,17 +1,16 @@
 source("B_H.R")
-lastH <- 860
+lastH <- 859
 #source(sprintf("./toFinal/toZ%d_H.R",workH))	# working
 
-load(sprintf("./save/Obj_allIdxLstZ%d.save",lateH) )
-load(sprintf("./save/Obj_fRstLstZ%d.save",lateH) )
-load(sprintf("./save/Obj_gEnvZ%d.save",lateH))
+load(sprintf("./save/Obj_allIdxLstZ%d.save",lastH) )
+load(sprintf("./save/Obj_fRstLstZ%d.save",lastH) )
+load(sprintf("./save/Obj_gEnvZ%d.save",lastH))
 
 names(fRstLst) <- names(allIdxLst$stdFiltedCnt)
 
 
 baseSpan <- 800:lastH
 stdFiltedCnt <- allIdxLst$stdFiltedCnt[as.character(baseSpan)]
-fRstLst <- fRstLst[as.character(baseSpan)]
 
 sfcHLst <- list(    sfcLate= baseSpan[length(baseSpan)] - 10:0
                     ,sfc0=as.integer(names(stdFiltedCnt)[stdFiltedCnt==0])
@@ -21,16 +20,20 @@ sfcHLst <- list(    sfcLate= baseSpan[length(baseSpan)] - 10:0
 
 for( sfnIdx in c("D0000.A","A0100.A","AP000.E") ){
     #   sfnIdx <- "D0000.A"
-    hSpan <- baseSpan[sapply( fRstLst ,function(p){ sfnIdx %in% p } )]
+    hSpan <- baseSpan[sapply( fRstLst[as.character(baseSpan)] ,function(p){ sfnIdx %in% p } )]
     hSpan.NG <- hSpan+1
     hSpan.NG <- hSpan.NG[hSpan.NG<=lastH]
     sfcHLst[[sprintf("NG:%s",sfnIdx)]] <- hSpan.NG
 }
 
 
-for( sfcIdx in names(sfcHLst) ){
-    hSpan <- sfcHLst[[sfcIdx]]
-    #   qqe working
+for( sfcIdx in names(sfcHLst) ){    # sfcIdx <- names(sfcHLst)[2]
+    for( hIdx in sfcHLst[[sfcIdx]] ){   # hIdx <- hSpan[1]
+        stdZoid <- gEnv$zhF[hIdx ,]
+        wEnv <- gEnv
+        wEnv$zhF <- gEnv$zhF[1:(hIdx-1),]
+        baseSpan[baseSpan<hIdx]
+    }
 }
 
 stdMI.grp <- bUtil.getStdMILst( gEnv ,fRstLst )
