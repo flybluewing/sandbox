7c0728dcc29801148127bbe803c0cd589169378f
krondor <krondor@example.com>
2019-08-07 18:22:40 +0900

    .

--- FlagBank/Bproject/B.R
+++ FlagBank/Bproject/B.R
@@ -1,28 +1,65 @@
 source("header.r")
 source("B_H.R")
-lastH <- 859
+lastH <- 859    # 최종 데이터의 로딩기준이지, 작업시점(workH)은 다를 수 있다.
 #source(sprintf("./toFinal/toZ%d_H.R",workH))	# working
 
 load(sprintf("./save/Obj_allIdxLstZ%d.save",lastH) )
 load(sprintf("./save/Obj_fRstLstZ%d.save",lastH) )
 load(sprintf("./save/Obj_gEnvZ%d.save",lastH))
 
-
 # Remove before flight
 #   B_H.R   -  경고! 실제 동작에서는 Q_RBF 해제할 것.
-hMtxLst <- B.makeHMtxLst( gEnv, allIdxLst, fRstLst )
-byFCol <- B.getHMtxLst_byFCol( hMtxLst )
 
-save( hMtxLst ,file=sprintf("./save/HMtxLst/Obj_hMtxLst_%d.save",hMtxLst$lastH) )
-#   load(sprintf("./save/HMtxLst/Obj_hMtxLst_%d.save",lastH))
+if( FALSE ){ # report sample
+    workH <- lastH-3    # workH 는 그때그때 필요에 따라.
+    hMtxLst <- B.makeHMtxLst( gEnv, allIdxLst, fRstLst, lastH=workH )
 
-B.rptHMtxLst( hMtxLst )
+    save( hMtxLst ,file=sprintf("./save/HMtxLst/Obj_hMtxLst_%d.save",hMtxLst$lastH) )
+    #   load(sprintf("./save/HMtxLst/Obj_hMtxLst_%d.save",lastH))
+    
+    B.rptHMtxLst( hMtxLst )
+    #   B.getHMtxLst_byFCol( ), B.getHMtxLst_byHIdx( ) 사용은
+    #   report 함수 내 코드 참조.
+}
 
-if(FALSE){
-    for( hIdx in (lastH-10:0) ){
-        hMtxLst <- B.makeHMtxLst( gEnv, allIdxLst, fRstLst, lastH=hIdx )
-        save( hMtxLst ,file=sprintf("./save/HMtxLst/Obj_hMtxLst_%d.save",hMtxLst$lastH) )
-    }
+if( FALSE ){    # stdZoid에 대한 cutting 시뮬레이션 예제 코드
+
+    configH <- lastH-20    # configH는 기본 cutting값을 얻기 위하는 시점에 따라 조절.
+    hMtxLst <- B.makeHMtxLst( gEnv, allIdxLst, fRstLst, lastH=configH )
+    stdCtrlCfgGrp <- bUtil.makeStdCtrlCfgGrp(hMtxLst)
+    save( stdCtrlCfgGrp ,file=sprintf("./save/HMtxLst/Obj_stdCtrlCfgGrp_%d.save",configH) )
+    #   load(sprintf("./save/HMtxLst/Obj_stdCtrlCfgGrp_%d.save",configH))
+    
+    for( curHIdx in (lastH- 15:0) ){
+
+        #   gEnv.w, allIdxLst.w, fRstLst.w 모두 curHIdx 시점에 맞게 변경 요.
+        gEnv.w <- gEnv
+        allIdxLst.w <- allIdxLst
+        fRstLst.w <- fRstLst
+
+        curHMtxLst <- B.makeHMtxLst( gEnv.w, allIdxLst.w, fRstLst.w, lastH=curHIdx )
+        cut.grp <- bFCust.getFCustGrp( stdCtrlCfgGrp )  # curHMtxLst 적용 추가 필요.
+
+        stdZoid <-  c( 4, 8,18,25,27,32)    # H860
+        #   stdZoid <- gEnv$zhF[nrow(gEnv$zhF),]
+        stdIdx <- k.getIdx_AllZoidMtx( gEnv, stdZoid )
+        fHName <- c( "sfcLate",   "NGD0000.A")  # fHName 분석하는 루틴 필요.
+
+        # wStdMI.grp <- bUtil.getStdMILst( gEnv ,fRstLst )
+        # wFilter.grp <- getFilter.grp( wStdMI.grp )
+        # wScoreMtx.grp <- getScoreMtx.grp.4H( stdZoid ,wFilter.grp )
+
+        # cutRst <- bUtil.cut( wScoreMtx.grp ,cut.grp ,fHName ,anaOnly=T )
+
+
+        # report example =================================================
+            # B.rptStdMI.grp( wStdMI.grp )
+            # B.rptScoreMtx.grp( wScoreMtx.grp )
+            # B.rptCut.grp( cut.grp )
+            # B.rptCutRst( cutRst )
+
+    } # curHIdx
+
 }
 
 
