e6e8bef49e0ea16f4a5bf92308db7b7e1102285b
krondor <krondor@example.com>
2019-09-24 23:51:46 +0900

    .

--- FlagBank/Bproject/C_H.R
+++ FlagBank/Bproject/C_H.R
@@ -3,12 +3,12 @@
 #   score를 생성함과 동시에 생존 여부 확인하여 필터링.
 #       그냥 scoreMtx를 만들면 getScoreMtx.grp() 함수가 부하량을 못 견딘다.
 #       return : surFlag
-C.primaryCut.bySC <- function( aZoidMtx ,filter.grp ,cut.grp ,logFile="primaryCut.bySC.txt" ){
+C.primaryCut.bySC <- function( allIdxF ,gEnv ,filter.grp ,cut.grp ,logFile="primaryCut.bySC.txt" ){
     #   aZoidMtx=gEnv$allZoidMtx[allIdxF,,drop=F]  ;scName="score2"    ;logFile="primaryCut.bySC.txt"
     logger=k.getFlogObj( sprintf("./log/%s",logFile) )
     logger$fLogStr("start", pTime=T ,pAppend=F )
 
-    aZoidLen <- nrow(allIdxMtx)
+    aZoidLen <- length(allIdxF)
     surFlag <- rep( F, aZoidLen )
 
     aZoidLen <- 37
