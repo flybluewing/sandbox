146b001a2d3c3551ef2ba007e8dd0fbc66540fa4
krondor <krondor@example.com>
2020-06-25 23:53:34 +0900

    .

--- FlagBank/Bproject/FCust_configExt.R
+++ FlagBank/Bproject/FCust_configExt.R
@@ -368,15 +368,47 @@ if( TRUE ){    # sample
     )
 }
 
+mName <- "score9"
+if( TRUE ){    # sample
+    scrExtMtxCfg[[mName]] <- list()
 
-
-
-
-
-
-
-
-
+    scrExtMtxCfg[[mName]]$filter01 <- list(
+        mName = mName   ,style=c( freqZero=TRUE )
+        ,fCol = list(
+            "xCntECnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 ,dimnames=list(c("min","max"),c("lev1","lev2")) )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        ) 
+            ,"xD2ECnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        )
+            ,"xDnECnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        )
+            ,"xLrECnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        )
+            ,"xRlECnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        )
+            ,"r2EvtCnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        )
+            ,"e2EvtCnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        )
+            ,"c2EvtCnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        )
+            ,"f2EvtCnt"=list( rng=matrix( c(0,1 ,0,2) ,ncol=2 )
+                            ,evt=matrix( c(c(3,3,3),c(2,3,4)) ,ncol=2)
+                        )
+        )
+        ,evtMax = NULL        ,rowReb = NULL        ,rowRebDup=NULL
+        ,summMtx = NULL       ,summMtx.reb = NULL   ,summMtx.sum = NULL
+        ,scMtx.sz = NULL      ,scMtx.sz.sum = NULL
+        ,isHard=NULL  # use default
+    )
+}
 
 
 
--- FlagBank/Bproject/lib/bFMtx_Ext.R
+++ FlagBank/Bproject/lib/bFMtx_Ext.R
@@ -507,3 +507,61 @@ if( TRUE ){
     bFMtxExtFltLst[[mName]]$filter01 <- fltCreater(mName)
 
 }
+
+mName <- "score9"
+if( FALSE ){
+
+    bFMtxExtFltLst[[mName]] <- list()
+        # rCnt rD2 rDn rLr rRl 
+        # eCnt eD2 eDn eLr eRl 
+        # cCnt cD2 cDn cLr cRl 
+        # fCnt fD2 fDn fLr fRl
+
+    fltCreater <- function( mName ){
+        fltObj <- list( mInfo=list() )
+        fltObj$mInfo$mName = mName
+        fltObj$mInfo$cName <- c("xCntECnt" 
+                                ,"xD2ECnt" ,"xDnECnt","xLrECnt","xRlECnt" 
+                                ,"r2EvtCnt" ,"e2EvtCnt" ,"c2EvtCnt" ,"f2EvtCnt" 
+        )
+
+        fltObj$getScore <- function( score ){
+            #   score <- scoreMtx.grp$basic$basic[[mName]]$scoreMtx[1,]
+            rVal <- rep(0,length(fltObj$mInfo$cName))  ;names(rVal) <- fltObj$mInfo$cName
+
+            scrEvt <- bFCust.getEvt( score ,scoreMtxCfg[[fltObj$mInfo$mName]]$fCol )["lev",]
+
+            rVal["xCntECnt"]<- sum( score[c("rCnt","eCnt","cCnt","fCnt")>=c(2,3,2,2)] )
+            rVal["xD2ECnt"] <- sum( score[c("rD2","eD2","cD2","fD2")>=c(2,2,2,2)] )
+            rVal["xDnECnt"] <- sum( score[c("rDn","eDn","cDn","fDn")>=c(2,2,2,2)] )
+            rVal["xLrECnt"] <- sum( score[c("rLr","eLr","cLr","fLr")>=c(2,2,2,2)] )
+            rVal["xRlECnt"] <- sum( score[c("rRl","eRl","cRl","fRl")>=c(2,2,2,2)] )
+
+            rVal["r2EvtCnt"]<- sum( score[c("rD2","rDn","rLr","rRl")] >= c(2,2,2,2) )
+            rVal["e2EvtCnt"]<- sum( score[c("eD2","eDn","eLr","eRl")] >= c(2,2,2,2) )
+            rVal["c2EvtCnt"]<- sum( score[c("cD2","cDn","cLr","cRl")] >= c(2,2,2,2) )
+            rVal["f2EvtCnt"]<- sum( score[c("fD2","fDn","fLr","fRl")] >= c(2,2,2,2) )
+
+            # rVal["xxx"]  <- sum(score[c("xxx","xxx","xxx")] ,na.rm=T)
+            # rVal["xxx"]  <- sum(scrEvt[c("xxx","xxx")] ,na.rm=T)
+
+            return( rVal )
+        }
+        fltObj$getScoreMtx <- function( scoreMtx ){
+            rMtx <- matrix( 0 ,nrow=nrow(scoreMtx) ,ncol=length(fltObj$mInfo$cName) )
+            colnames(rMtx) <- fltObj$mInfo$cName
+
+            for( rIdx in seq_len(nrow(rMtx)) ){
+                rMtx[rIdx,] <- fltObj$getScore( scoreMtx[rIdx,] )
+            }
+            rownames(rMtx) <- rownames(scoreMtx)
+
+            return( rMtx )
+        }
+
+        return(fltObj)
+    }
+    bFMtxExtFltLst[[mName]]$filter01 <- fltCreater(mName)
+
+}
+
