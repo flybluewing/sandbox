7c4e0037525b08d894f52a15653633475af50a0d
krondor <krondor@example.com>
2020-08-02 07:32:40 +0900

    .

--- FlagBank/Bproject/B_prll_H.R
+++ FlagBank/Bproject/B_prll_H.R
@@ -2,10 +2,25 @@
 
 
 
-Bprll.cutRstLst <- function( cutRstLst ,tgt.scMtx ){
+Bprll.resultLst <- function( resultLst ,tgt.scMtx ){
 
-#   cutRstLst[[1]]$auxInfoLst$basic$score1
-#       "fColEvt"     "summMtx"     "summMtx.reb" "scMtx.sz"   
+    mtx <- matrix( " ", nrow=length(resultLst) ,ncol=length(tgt.scMtx) 
+            ,dimnames=list( names(resultLst) ,tgt.scMtx )
+    )
+    auxTestLst <- lapply( resultLst ,function(p){p$auxTest})
+    for( idx in 1:length(resultLst) ){
+        zeroM <- resultLst[[idx]]$auxTest$zeroM
+        mtx[ idx, intersect( colnames(mtx) ,zeroM ) ] <- "T"
+    }
+
+
+
+    #   cutRstLst[[1]]$auxInfoLst$basic$score1
+    #       "fColEvt"     "summMtx"     "summMtx.reb" "scMtx.sz"   
+    names( resultLst ) <- sapply( resultLst ,function(p){p$hIdx})
+    cutRstLst <- lapply( resultLst ,function(p){p$cutRst})
+    names(cutRstLst) <- paste("H",testSpan,sep="")
+    names(cutRstLst) <- paste( names(cutRstLst) ,allIdxLst$stdFiltedCnt[as.character(testSpan)] ,sep="_" )
 
     if( is.null(tgt.scMtx) ){
         tgt.scMtx <- names( cutRstLst[[1]]$auxInfoLst$basic )
@@ -15,6 +30,7 @@ Bprll.cutRstLst <- function( cutRstLst ,tgt.scMtx ){
     rownames(hSummMtx) <- names(cutRstLst)   ;colnames(hSummMtx) <- tgt.scMtx
     for( idx in 1:length(cutRstLst) ){
         for( mName in tgt.scMtx ){
+            rawObj <- 
             summMtx <- cutRstLst[[idx]]$auxInfoLst$basic[[mName]]$summMtx
             #     all ph fCol phReb xyCnt.fCol xyCnt.phase
             # raw   0  0    0     0          0           0
---
+++ FlagBank/Bproject/lib/bCMtx.R
@@ -0,0 +1,14 @@
+#   cutRst1Score <- bUtil.getCut1Score( scoreMtx.grp ,cut.grp ,fHName ,tgt.scMtx=tgt.scMtx )
+
+bCMtxLst <- list()
+
+
+mfName <- "crScrN"  # Cut-Result, Score N
+if( TRUE ){
+
+
+
+
+}
+
+
