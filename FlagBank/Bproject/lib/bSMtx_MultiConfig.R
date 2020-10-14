694488365c11b478ed9e1c9d116ca7ce456b7cbc
krondor <krondor@example.com>
2020-10-14 12:40:10 +0900

    .

---
+++ FlagBank/Bproject/lib/bSMtx_MultiConfig.R
@@ -0,0 +1,20 @@
+
+bSMtxMCfg <- list()
+
+bSMName <- "bbb"
+if( TRUE ){
+    bSMtxMCfg[[crMName]] <- list(  mName = crMName
+        ,fCol = list(
+              "hpn"  = list( rng=c(min=0,max=2) )
+             ,"ph"   = list( rng=c(min=0,max=1) )
+        )
+    )
+}
+
+for( idx in seq_len(length(bSMtxMCfg)) ){
+
+}
+
+
+
+
