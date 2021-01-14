336dce0576fea0f93914a71210371e4205358503
krondor <krondor@example.com>
2021-01-15 02:35:15 +0900

    .

--- FlagBank/Bproject/lib/bHCRMtx.R
+++ FlagBank/Bproject/lib/bHCRMtx.R
@@ -2,10 +2,12 @@
 
 bHCRMtxLst <- list()
 
+crScrH <- crScrHTool$getData()
 
 mName <- "HCRsz_bf01Ph"
 if( FALSE ){
-    wMLst=NULL
+    workMName <- c("score1","score2","sScore01","sScore02","fake")
+    wMLst= HCR.getMName( workMName ,crScrH ,warn=T )
     bHCRMtxLst[[mName]] <- HCR.MtxTmpl_szReb( mName=mName ,wMLst=wMLst ,szColName="ph" )
 }
 
--- FlagBank/Bproject/lib/bHCRMtx_Util.R
+++ FlagBank/Bproject/lib/bHCRMtx_Util.R
@@ -1,16 +1,26 @@
-#
-HCR.getMName <- function( workMName ,crScrH ){
+
+HCR.getMName <- function( workMName ,crScrH ,warn=T ){
     # bFMtx에 해당하는 mName과 bSMtx에 해당하는 mName 분류
 
-}
+    bfMNames <- intersect( workMName ,names(crScrH$std.grp[[1]]$sfcLate$basic) )
+    bSMNames <- intersect( workMName ,names(crScrH$bS.grp[[1]]$sfcLate$basic) )
 
+    if( warn ){
+        haveFlag <- (workMName %in% bfMNames) | (workMName %in% bSMNames)
+        if( any(!haveFlag) ){
+            missingM <- workMName[!haveFlag]
+            cat(sprintf("    Warning!! missing in crScrH : %s \n",paste(missingM,collapse=",")))
+        }
+    }
+
+    return( list(bf=bfMNames ,bS=bSMNames) )
+}
 
 
 HCR.MtxTmpl_szReb <- function( mName ,wMLst ,szColName ){
     #   HCR.getMName(tgt.scMtx ,crScrH)     # bFMtx, bSMtx에 따라 분리추출된 mName
 
-	rObj <- list( 	idStr=idStr ,wMLst=wMLst
-                    ,mInfo=c("mName"=mName ,"szColName"=szColName )
+	rObj <- list( 	mInfo=c("mName"=mName ,"szColName"=szColName ) ,wMLst=wMLst
 				)
 
     rObj$fMtxObj <- function( crLst ){
