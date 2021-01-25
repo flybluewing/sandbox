bbb17d446e6b2335277090b3133c5440720b495d
krondor <krondor@example.com>
2021-01-26 02:27:47 +0900

    .

--- FlagBank/Bproject/lib/bHCRMtx.R
+++ FlagBank/Bproject/lib/bHCRMtx.R
@@ -11,16 +11,16 @@ if( TRUE ){
     #                 rebDup    0      0           0    0      0           0
 
     #   mName 별 중복 카운트 수만 비교할 뿐, 중복 발생 phase를 비교하는 것은 아님을 유의.
-    workMName <- c("score1","score2","sScore01","sScore02","fake")
-    wMLst= HCR.getMName( workMName ,crScrH ,warn=T )
+    workMName <- c("score1","score2","sScore01","sScore02")
+    wMLst= HCR.getMName( workMName ,crScrH ,warn=F )
     bHCRMtxLst[[mName]] <- HCR.MtxTmpl_szReb( mName=mName ,wMLst=wMLst ,szColName="r.ph" ,szRowName="rebCnt" )
 }
 
 mName <- "HCRsz_bf01fCol"
 if( TRUE ){
     #   mName 별 중복 카운트 수만 비교할 뿐, 중복 발생 fCol을 비교하는 것은 아님을 유의.
-    workMName <- c("score1","score2","sScore01","sScore02","fake")
-    wMLst= HCR.getMName( workMName ,crScrH ,warn=T )
+    workMName <- c("score1","score2","sScore01","sScore02")
+    wMLst= HCR.getMName( workMName ,crScrH ,warn=F )
     bHCRMtxLst[[mName]] <- HCR.MtxTmpl_szReb( mName=mName ,wMLst=wMLst ,szColName="r.fCol" ,szRowName="rebCnt" )
 }
 
--- FlagBank/Bproject/lib/bHCRMtx_Util.R
+++ FlagBank/Bproject/lib/bHCRMtx_Util.R
@@ -16,6 +16,104 @@ HCR.getMName <- function( workMName ,crScrH ,warn=T ){
     return( list(bf=bfMNames ,bS=bSMNames) )
 }
 
+HCR.getHCRNames <- function( tgt.scMtx=NULL ){
+
+    tgt.HCRMtx <- character(0)
+    if( !is.null(tgt.scMtx) ){
+        for( mName in names(bHCRMtxLst) ){
+            flag_bf <- all( bHCRMtxLst[[mName]]$wMLst$bf %in% tgt.scMtx )
+            flag_bS <- all( bHCRMtxLst[[mName]]$wMLst$bS %in% tgt.scMtx )
+
+            if( flag_bf && flag_bS ){
+                tgt.HCRMtx <- c( tgt.HCRMtx ,mName )
+            }
+        }
+    } else {
+        tgt.HCRMtx <- names(bHCRMtxLst)
+    }
+
+    return( tgt.HCRMtx )
+}
+
+HCR.getFilter.grp <- function( tgt.scMtx=NULL ){   # tgt.scMtx
+    #   bFMtx.R getFilter.grp() 참고.
+
+    tgt.HCRMtx <- HCR.getHCRNames( tgt.scMtx )
+
+    return( bHCRMtxLst[tgt.HCRMtx] )
+
+}
+
+HCR.makeHCRMtxLst <- function( crScrH ,allIdxLst ,fRstLst ,lastH=NULL ,tgt.scMtx=NULL ){
+    # 파라미터
+    #   crScrH <- crScrHTool$getData()
+    # 참고
+    #   B.makeHMtxLst( gEnv, allIdxLst, fRstLst, lastH=lastH, tgt.scMtx ) 참고
+
+    baseSpan <- as.integer(names(crScrH$stdIdx))
+    baseSpan.len <- length(baseSpan)
+    if( any(baseSpan[1:(baseSpan.len-1)]!=(baseSpan[2:baseSpan.len]-1)) ){
+        return( "Error. check crScrH. missing step found." )
+    }
+
+    if( is.null(lastH) ){   lastH <- baseSpan[baseSpan.len]
+    } else {
+        fndIdx <- which(baseSpan==lastH)
+        if( 0<length(fndIdx) ){
+            baseSpan <- baseSpan[1:fndIdx]
+        } else {    return( "Error. check crScrH range for lastH" ) }
+    }
+
+    fRstLst.hSpan <- as.integer(names(fRstLst)[1]):lastH
+    fRstLst <- fRstLst[as.character(fRstLst.hSpan)]
+    
+
+    sfcHLst <- bUtil.getSfcHLst( stdFiltedCnt=allIdxLst$stdFiltedCnt[as.character(baseSpan)] ,baseSpan ,fRstLst )
+
+    scoreMtxLst <- list()
+    for( sfcIdx in names(sfcHLst) ){
+
+        scoreMtx.grp.lst <- list( )
+        for( hIdx in sfcHLst[[sfcIdx]] ){
+
+            # hSpanStr <- as.character( sfcHLst[[sfcIdx]] )
+            # crScrH.w <- crScrH
+            # crScrH.w$stdIdx     <- crScrH.w$stdIdx[hSpanStr]
+            # crScrH.w$std.grp    <- crScrH.w$std.grp[hSpanStr]
+            # crScrH.w$bS.grp     <- crScrH.w$bS.grp[hSpanStr]
+
+            filterLst <- HCR.getFilter.grp()
+            # HCR.getScoreMtx.grp
+
+            # stdZoid <- gEnv$zhF[hIdx ,]
+            # wEnv <- gEnv
+            # wEnv$zhF <- gEnv$zhF[1:(hIdx-1),]
+
+            # fRstLst.w <- fRstLst[as.character(fRstLst.hSpan[fRstLst.hSpan<hIdx])]
+
+            # working
+            # stdMI.grp <- bUtil.getStdMILst( wEnv ,fRstLst.w )
+            # filter.grp <- getFilter.grp( stdMI.grp ,tgt.scMtx )
+
+            # scoreMtx.grp <- getScoreMtx.grp.4H( stdZoid ,filter.grp )
+            # scoreMtx.grp.lst[[sprintf("hIdx:%d",hIdx)]] <- scoreMtx.grp
+        }
+    }
+
+
+
+
+
+}
+
+HCR.getScoreMtx.grp <- function( ){
+
+}
+
+HCR.getCutterGrp <- function( hCRLst ,tgt.scMtx ){
+    # bFCust.getFCustGrp( hMtxLst ,tgt.scMtx ) 참고.
+
+}
 
 HCR.MtxTmpl_szReb <- function( mName ,wMLst ,szColName ,szRowName ){
     #   HCR.getMName(tgt.scMtx ,crScrH)     # bFMtx, bSMtx에 따라 분리추출된 mName
